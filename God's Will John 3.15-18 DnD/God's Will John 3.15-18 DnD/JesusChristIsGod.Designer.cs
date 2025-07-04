479c18fd61ceadeb59c275b980e80b4831f89f24
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2025-07-04 08:51:34 -0500

    Add files via upload

---
+++ God's Will John 3.15-18 DnD/God's Will John 3.15-18 DnD/JesusChristIsGod.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace God_s_Will_John_3._15_18_DnD
+{
+    partial class JesusChristIsGod
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSin = new System.Windows.Forms.Label();
+            this.boxMessage = new System.Windows.Forms.ListBox();
+            this.BtnRoll = new System.Windows.Forms.Button();
+            this.BtnAccept = new System.Windows.Forms.Button();
+            this.BtnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSin
+            // 
+            this.lblSin.AutoSize = true;
+            this.lblSin.Font = new System.Drawing.Font("Arial", 26F, System.Drawing.FontStyle.Bold);
+            this.lblSin.ForeColor = System.Drawing.Color.Red;
+            this.lblSin.Location = new System.Drawing.Point(25, 28);
+            this.lblSin.Name = "lblSin";
+            this.lblSin.Size = new System.Drawing.Size(408, 51);
+            this.lblSin.TabIndex = 0;
+            this.lblSin.Text = "Current Sin Score:";
+            // 
+            // boxMessage
+            // 
+            this.boxMessage.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxMessage.FormattingEnabled = true;
+            this.boxMessage.ItemHeight = 23;
+            this.boxMessage.Location = new System.Drawing.Point(34, 101);
+            this.boxMessage.Name = "boxMessage";
+            this.boxMessage.Size = new System.Drawing.Size(784, 234);
+            this.boxMessage.TabIndex = 1;
+            // 
+            // BtnRoll
+            // 
+            this.BtnRoll.BackColor = System.Drawing.Color.LawnGreen;
+            this.BtnRoll.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRoll.Location = new System.Drawing.Point(34, 370);
+            this.BtnRoll.Name = "BtnRoll";
+            this.BtnRoll.Size = new System.Drawing.Size(138, 43);
+            this.BtnRoll.TabIndex = 2;
+            this.BtnRoll.Text = "Roll";
+            this.BtnRoll.UseVisualStyleBackColor = false;
+            this.BtnRoll.Click += new System.EventHandler(this.BtnRollClickEvent);
+            // 
+            // BtnAccept
+            // 
+            this.BtnAccept.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BtnAccept.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAccept.Location = new System.Drawing.Point(336, 370);
+            this.BtnAccept.Name = "BtnAccept";
+            this.BtnAccept.Size = new System.Drawing.Size(169, 43);
+            this.BtnAccept.TabIndex = 3;
+            this.BtnAccept.Text = "Accept Jesus";
+            this.BtnAccept.UseVisualStyleBackColor = false;
+            this.BtnAccept.Click += new System.EventHandler(this.BtnAcceptClickEvent);
+            // 
+            // BtnQuit
+            // 
+            this.BtnQuit.BackColor = System.Drawing.Color.Red;
+            this.BtnQuit.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnQuit.Location = new System.Drawing.Point(680, 370);
+            this.BtnQuit.Name = "BtnQuit";
+            this.BtnQuit.Size = new System.Drawing.Size(138, 43);
+            this.BtnQuit.TabIndex = 4;
+            this.BtnQuit.Text = "Quit";
+            this.BtnQuit.UseVisualStyleBackColor = false;
+            this.BtnQuit.Click += new System.EventHandler(this.BtnQuitClickEvent);
+            // 
+            // JesusChristIsGod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 543);
+            this.Controls.Add(this.BtnQuit);
+            this.Controls.Add(this.BtnAccept);
+            this.Controls.Add(this.BtnRoll);
+            this.Controls.Add(this.boxMessage);
+            this.Controls.Add(this.lblSin);
+            this.Name = "JesusChristIsGod";
+            this.Text = "John 3:15-18";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSin;
+        private System.Windows.Forms.ListBox boxMessage;
+        private System.Windows.Forms.Button BtnRoll;
+        private System.Windows.Forms.Button BtnAccept;
+        private System.Windows.Forms.Button BtnQuit;
+    }
+}
+
---
+++ God's Will John 3.15-18 DnD/God's Will John 3.15-18 DnD/JesusChristIsGod.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace God_s_Will_John_3._15_18_DnD
+{
+    // This is the main form class for the Christian-themed dice game
+    public partial class JesusChristIsGod : Form
+    {
+        // Random number generator for dice rolls
+        private Random random = new Random();
+
+        // Tracks the current "Sin Score"
+        private int sinScore = 1;
+
+        // Indicates if the player has won by accepting Jesus
+        private bool isWinner = false;
+
+        // Constructor: Initializes form and displays welcome messages
+        public JesusChristIsGod()
+        {
+            InitializeComponent();
+            UpdateSinLabel();
+            ShowMessage("Welcome to 'John 3:15-18' - A Christian Dice Rolling Game!");
+            ShowMessage("Click 'Roll' to roll the D20, 'Accept Jesus', or 'Quit'.");
+        }
+
+        // Event handler for the 'Roll' button click
+        private void BtnRollClickEvent(object sender, EventArgs e)
+        {
+            // Prevent rolling if the game is already won or sin score is 100+
+            if (isWinner || sinScore >= 100) return;
+            RollD20();
+        }
+
+        // Event handler for the 'Accept Jesus' button click
+        private void BtnAcceptClickEvent(object sender, EventArgs e)
+        {
+            AcceptJesus();
+        }
+
+        // Event handler for the 'Quit' button click
+        private void BtnQuitClickEvent(object sender, EventArgs e)
+        {
+            ShowMessage("You chose to give up... Please reconsider accepting Jesus before it's too late.");
+            ShowMessage("John 3:17: 'For God did not send his Son into the world to condemn the world, but to save the world through him.'");
+            ShowMessage("John 3:18: 'Whoever believes in him is not condemned, but whoever does not believe stands condemned already. .");
+            ShowMessage("because they have not believed in the name of God’s one and only Son.'");
+            Application.Exit(); // Closes the application
+        }
+
+        // Rolls a 20-sided dice and processes the result
+        private void RollD20()
+        {
+            int roll = random.Next(1, 21); // Roll a number between 1 and 20
+            ShowMessage($"You rolled a D20: {roll}");
+
+            // Instant win if a 20 is rolled
+            if (roll == 20)
+            {
+                ShowMessage("You accepted Jesus Christ as your Lord and Savior!");
+                isWinner = true;
+                EndGameWin();
+                return;
+            }
+
+            // Rolling a 1 automatically triggers acceptance
+            if (roll == 1)
+            {
+                ShowMessage("You rolled a 1. It's time to repent and turn to Jesus Christ.");
+                AcceptJesus();
+                return;
+            }
+
+            // Add to sin score on certain odd-numbered rolls
+            if (new[] { 19, 18, 17, 16, 15, 14, 13, 11, 9, 7, 5, 3, 2 }.Contains(roll))
+            {
+                sinScore += roll;
+                ShowMessage($"Sin increased by {roll}. New Sin Score: {sinScore}");
+            }
+            else
+            {
+                // For even-numbered rolls, perform a saving throw
+                switch (roll)
+                {
+                    case 12:
+                    case 10:
+                    case 8:
+                    case 6:
+                    case 4:
+                        SavingThrow(roll);
+                        break;
+                    default:
+                        ShowMessage("Nothing happened this turn.");
+                        break;
+                }
+            }
+
+            // End game if sin score reaches 100 or more
+            if (sinScore >= 100)
+            {
+                ShowMessage("You reached a sin score of 100. Game Over.");
+                ShowMessage("Before it's too late, turn to Jesus! Accept Him as your Lord and Savior.");
+                ShowMessage("John 3:17: 'For God did not send his Son into the world to condemn the world, but to save the world through him.'");
+                ShowMessage("John 3:18: 'Whoever believes in him is not condemned, but whoever does not believe stands condemned already. .");
+                ShowMessage("because they have not believed in the name of God’s one and only Son.'");
+            }
+
+            UpdateSinLabel();
+        }
+
+        // Performs a saving throw that reduces the sin score
+        private void SavingThrow(int dieSize)
+        {
+            int savingThrow = random.Next(1, dieSize + 1); // Roll a die of specified size
+            sinScore -= savingThrow;
+            if (sinScore <= 0) sinScore = 1; // Ensure sin score doesn't go below 1
+            ShowMessage($"Saving throw D{dieSize} rolled: {savingThrow}. Sin reduced. New Sin Score: {sinScore}");
+        }
+
+        // Called when the user accepts Jesus; resets sin and declares victory
+        private void AcceptJesus()
+        {
+            ShowMessage("You accepted Jesus Christ as God, King, your Lord and Savior!");
+            ShowMessage("Your sins are forgiven. You are a winner through Christ.");
+            sinScore = 0;
+            isWinner = true;
+            EndGameWin();
+            UpdateSinLabel();
+        } 
+
+        // Displays winning messages and relevant scripture
+        private void EndGameWin()
+        {
+            ShowMessage("Congratulations! You have eternal life through Jesus Christ!");
+            ShowMessage("John 3:15: 'That whoever believes in Him should not perish but have eternal life.'");
+            ShowMessage("John 3:16: 'For God so loved the world that he gave his one and only Son,");
+            ShowMessage("that whoever believes in him shall not perish but have eternal life.'");
+        }
+
+        // Updates the label showing the current sin score
+        private void UpdateSinLabel()
+        {
+            lblSin.Text = $"Current Sin Score: {sinScore}";
+        }
+
+        // Appends a message to the list box and scrolls to the latest entry
+        private void ShowMessage(string message)
+        {
+            boxMessage.Items.Add(message);
+            boxMessage.TopIndex = boxMessage.Items.Count - 1;
+        }
+    }
+}
